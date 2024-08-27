ce5f08f4225cf93b2ed2f7234c290aef33e0cda3
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-27 14:37:32 +0200

    Add menu item everywhere.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,26 +2,28 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Entity\Client;
+use App\Entity\ContratDeMaintenance;
+use App\Entity\Facture;
+use App\Entity\Relance;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
-    public function __construct(private AdminUrlGenerator $adminUrlGenerator)
-    {
-        
-    }
+    public function __construct(private AdminUrlGenerator $adminUrlGenerator) {}
 
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $url = $this->adminUrlGenerator->setController(ClientCrudController::class)->generateUrl();
 
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -32,7 +34,28 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Clients');
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter un client', 'fas fa-plus', Client::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des clients', 'fas fa-eye', Client::class),
+        ]);
+
+        yield MenuItem::section('Contrats');
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter un contrat', 'fas fa-plus', ContratDeMaintenance::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des contrats', 'fas fa-eye', ContratDeMaintenance::class),
+        ]);
+
+        yield MenuItem::section('Factures');
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter une facture', 'fas fa-plus', Facture::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des factures', 'fas fa-eye', Facture::class),
+        ]);
+
+        yield MenuItem::section('Relances');
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter une relance', 'fas fa-plus', Relance::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des relances', 'fas fa-eye', Relance::class),
+        ]);
     }
 }
