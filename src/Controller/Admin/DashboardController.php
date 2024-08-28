ef7b8223eb2e4c09b4cc885351fc5586368da6ca
Flibouche <kevin.pfiffer2@gmail.com>
2024-08-28 14:04:17 +0200

    Add verify email bundle, add registration form and controller.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,7 +18,7 @@ class DashboardController extends AbstractDashboardController
 {
     public function __construct(private AdminUrlGenerator $adminUrlGenerator) {}
 
-    #[Route('/admin', name: 'admin')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
         $url = $this->adminUrlGenerator->setController(ClientCrudController::class)->generateUrl();
